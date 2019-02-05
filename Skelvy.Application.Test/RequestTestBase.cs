d29af61c7dc55b2955295626ebf5eeaa7516be8d
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-02-05 15:42:18 +0100

    feature(tests) add request test base class

---
+++ Skelvy.Application.Test/RequestTestBase.cs
@@ -0,0 +1,46 @@
+using System;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Skelvy.Application.Core.Mapping;
+using Skelvy.Persistence;
+
+namespace Skelvy.Application.Test
+{
+  public abstract class RequestTestBase
+  {
+    protected static SkelvyContext DbContext(bool sqlLite = true)
+    {
+      var builder = new DbContextOptionsBuilder<SkelvyContext>();
+
+      if (sqlLite)
+      {
+        builder.UseSqlite("DataSource=:memory:");
+      }
+      else
+      {
+        builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+      }
+
+      var dbContext = new SkelvyContext(builder.Options);
+
+      if (sqlLite)
+      {
+        dbContext.Database.OpenConnection();
+      }
+
+      dbContext.Database.EnsureCreated();
+
+      return dbContext;
+    }
+
+    protected static IMapper Mapper()
+    {
+      var mapperConfiguration = new MapperConfiguration(configuration =>
+      {
+        configuration.AddProfile(new MappingProfile());
+      });
+
+      return mapperConfiguration.CreateMapper();
+    }
+  }
+}
