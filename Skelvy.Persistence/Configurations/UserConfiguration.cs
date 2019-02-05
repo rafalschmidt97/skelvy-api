bd8cfdbceda0d5de53144f6a2e4ff98da94f0444
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-02-05 15:51:34 +0100

    feature(users) add user context configuration
    - update context
    - update initializer
    - add configuration
    - add migration

---
+++ Skelvy.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Skelvy.Domain.Entities;
+
+namespace Skelvy.Persistence.Configurations
+{
+  public class UserConfiguration : IEntityTypeConfiguration<User>
+  {
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+      builder.Property(e => e.Email).IsRequired().HasMaxLength(60);
+      builder.Property(e => e.Name).IsRequired().HasMaxLength(60);
+    }
+  }
+}
