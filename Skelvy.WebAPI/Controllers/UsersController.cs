20b513135f16485d6045d24073a15bd4a547d24f
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-02-05 20:07:42 +0100

    bugfix(users) change task signature in controller

--- Skelvy.WebAPI/Controllers/UsersController.cs
+++ Skelvy.WebAPI/Controllers/UsersController.cs
@@ -2,33 +2,30 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Skelvy.Application.Users.Commands.CreateUser;
+using Skelvy.Application.Users.Queries;
 using Skelvy.Application.Users.Queries.GetUserDetail;
 using Skelvy.Application.Users.Queries.GetUsers;
-using Skelvy.Domain.Entities;
 
 namespace Skelvy.WebAPI.Controllers
 {
   public class UsersController : BaseController
   {
     [HttpGet]
-    public async Task<ActionResult<ICollection<User>>> GetAll()
+    public async Task<ICollection<UserDto>> GetAll()
     {
-      var users = await Mediator.Send(new GetUsersQuery());
-      return Ok(users);
+      return await Mediator.Send(new GetUsersQuery());
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<ICollection<User>>> Get(int id)
+    public async Task<UserDto> Get(int id)
     {
-      var user = await Mediator.Send(new GetUserDetailQuery { Id = id });
-      return Ok(user);
+      return await Mediator.Send(new GetUserDetailQuery { Id = id });
     }
 
     [HttpPost]
-    public async Task<ActionResult<ICollection<User>>> Add(CreateUserCommand request)
+    public async Task<double> Add(CreateUserCommand request)
     {
-      var userId = await Mediator.Send(request);
-      return Ok(userId);
+      return await Mediator.Send(request);
     }
   }
 }
