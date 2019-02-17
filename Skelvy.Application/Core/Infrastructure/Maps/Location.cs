b2b697667eb94d0b378105e0f5fe018ef65dd187
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-02-17 21:27:58 +0100

    feature(infrastructure) add google maps service

---
+++ Skelvy.Application/Core/Infrastructure/Maps/Location.cs
@@ -0,0 +1,13 @@
+namespace Skelvy.Application.Core.Infrastructure.Maps
+{
+  public class Location
+  {
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Country { get; set; }
+    public string State { get; set; }
+    public string District { get; set; }
+    public string City { get; set; }
+    public string Type { get; set; }
+  }
+}
