f010085ca545bae64e7b4b28e14f5da6e3a70d6b
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-02-22 17:56:38 +0100

    refactor(meetings) move utilities outside of create meeting request

--- Skelvy.Application/Meetings/Commands/CreateMeetingRequest/CreateMeetingRequestCommandHandler.cs
+++ Skelvy.Application/Meetings/Commands/CreateMeetingRequest/CreateMeetingRequestCommandHandler.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -9,6 +8,7 @@ using Skelvy.Application.Core.Exceptions;
 using Skelvy.Common;
 using Skelvy.Domain.Entities;
 using Skelvy.Persistence;
+using static Skelvy.Application.Meetings.Commands.CreateMeetingRequest.CreateMeetingRequestHelper;
 
 namespace Skelvy.Application.Meetings.Commands.CreateMeetingRequest
 {
@@ -33,12 +33,7 @@ namespace Skelvy.Application.Meetings.Commands.CreateMeetingRequest
       }
       else
       {
-        var existingRequest = await FindMatchingMeetingRequest(newRequest, data.user, cancellationToken);
-
-        if (existingRequest != null)
-        {
-          await CreateNewMeeting(newRequest, existingRequest, cancellationToken);
-        }
+        await MatchExistingMeetingRequests(newRequest, data.user, cancellationToken);
       }
 
       return Unit.Value;
@@ -56,7 +51,7 @@ namespace Skelvy.Application.Meetings.Commands.CreateMeetingRequest
       }
 
       var drinks = await _context.Drinks.ToListAsync(cancellationToken);
-      var filteredDrinks = drinks.FindAll(x => request.Drinks.Any(y => y.Id == x.Id));
+      var filteredDrinks = drinks.Where(x => request.Drinks.Any(y => y.Id == x.Id)).ToList();
 
       if (filteredDrinks.Count != request.Drinks.Count)
       {
@@ -123,7 +118,7 @@ namespace Skelvy.Application.Meetings.Commands.CreateMeetingRequest
         .ThenInclude(x => x.Drink)
         .ToListAsync(cancellationToken);
 
-      return meetings.Find(x => IsMeetingMatchRequest(x, newRequest, user));
+      return meetings.FirstOrDefault(x => IsMeetingMatchRequest(x, newRequest, user));
     }
 
     private static bool IsMeetingMatchRequest(Meeting meeting, MeetingRequest newRequest, User user)
@@ -160,6 +155,19 @@ namespace Skelvy.Application.Meetings.Commands.CreateMeetingRequest
       await _context.SaveChangesAsync(cancellationToken);
     }
 
+    private async Task MatchExistingMeetingRequests(
+      MeetingRequest newRequest,
+      User user,
+      CancellationToken cancellationToken)
+    {
+      var existingRequest = await FindMatchingMeetingRequest(newRequest, user, cancellationToken);
+
+      if (existingRequest != null)
+      {
+        await CreateNewMeeting(newRequest, existingRequest, cancellationToken);
+      }
+    }
+
     private async Task<MeetingRequest> FindMatchingMeetingRequest(
       MeetingRequest newRequest,
       User user,
@@ -172,7 +180,7 @@ namespace Skelvy.Application.Meetings.Commands.CreateMeetingRequest
         .ThenInclude(x => x.Drink)
         .ToListAsync(cancellationToken);
 
-      return requests.Find(x => AreRequestsMatch(x, newRequest, user));
+      return requests.FirstOrDefault(x => AreRequestsMatch(x, newRequest, user));
     }
 
     private static bool AreRequestsMatch(
@@ -242,57 +250,6 @@ namespace Skelvy.Application.Meetings.Commands.CreateMeetingRequest
         MeetingRequestId = request.Id,
         DrinkId = requestDrink.Id
       });
-    }
-
-    private static double CalculateDistance(double latitude1, double longitude1, double latitude2, double longitude2)
-    {
-      var d1 = latitude1 * (Math.PI / 180.0);
-      var num1 = longitude1 * (Math.PI / 180.0);
-      var d2 = latitude2 * (Math.PI / 180.0);
-      var num2 = longitude2 * (Math.PI / 180.0) - num1;
-      var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) +
-               Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
-
-      return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3))) / 1000;
-    }
-
-    private static int CalculateAge(DateTime date)
-    {
-      var age = DateTime.Now.Year - date.Year;
-      if (DateTime.Now.DayOfYear < date.DayOfYear)
-      {
-        age = age - 1;
-      }
-
-      return age;
-    }
-
-    private static DateTime FindCommonDate(MeetingRequest request1, MeetingRequest request2)
-    {
-      var dates = new List<DateTime>();
-
-      for (var i = request1.MinDate; i <= request1.MaxDate; i = i.AddDays(1))
-      {
-        dates.Add(i);
-      }
-
-      var commonDates = new List<DateTime>();
-
-      foreach (var date in dates)
-      {
-        if (date >= request1.MinDate && date < request1.MaxDate &&
-              date >= request2.MinDate && date < request2.MaxDate)
-        {
-          commonDates.Add(date);
-        }
-      }
-
-      return dates.First();
-    }
-
-    private static int FindCommonDrink(MeetingRequest request1, MeetingRequest request2)
-    {
-      return request1.Drinks.First(x => request2.Drinks.Any(y => y.DrinkId == x.DrinkId)).DrinkId;
     }
   }
 }
