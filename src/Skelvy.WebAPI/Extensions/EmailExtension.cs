e76d118557f3ee43a42f791256c925b1304fc559
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-03-26 11:44:48 +0100

    feature(infrastructure) fix razor views by removing layout

--- src/Skelvy.WebAPI/Extensions/EmailExtension.cs
+++ src/Skelvy.WebAPI/Extensions/EmailExtension.cs
@@ -1,11 +1,7 @@
 using System.Net;
 using System.Net.Mail;
-using System.Threading.Tasks;
-using FluentEmail.Core.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Skelvy.WebAPI.Infrastructure.Emails;
 
 namespace Skelvy.WebAPI.Extensions
 {
@@ -15,10 +11,8 @@ namespace Skelvy.WebAPI.Extensions
     {
       services
         .AddFluentEmail(configuration["Email:Username"], configuration["Email:Name"])
-        .AddSmtpSender(PrepareSmtpClientSsl(configuration));
-
-      services.TryAdd(ServiceDescriptor.Singleton<IRazorViewToStringRenderer, RazorViewToStringRenderer>());
-      services.TryAdd(ServiceDescriptor.Singleton<ITemplateRenderer, RazorFixRenderer>());
+        .AddSmtpSender(PrepareSmtpClientSsl(configuration))
+        .AddRazorRenderer();
     }
 
     private static SmtpClient PrepareSmtpClientSsl(IConfiguration configuration)
@@ -29,26 +23,6 @@ namespace Skelvy.WebAPI.Extensions
         DeliveryMethod = SmtpDeliveryMethod.Network,
         Credentials = new NetworkCredential(configuration["Email:Username"], configuration["Email:Password"])
       };
-    }
-  }
-
-  public class RazorFixRenderer : ITemplateRenderer
-  {
-    private readonly IRazorViewToStringRenderer _renderer;
-
-    public RazorFixRenderer(IRazorViewToStringRenderer renderer)
-    {
-      _renderer = renderer;
-    }
-
-    public async Task<string> ParseAsync<T>(string template, T model, bool isHtml = true)
-    {
-      return await _renderer.RenderViewToStringAsync(template, model);
-    }
-
-    public string Parse<T>(string template, T model, bool isHtml = true)
-    {
-      return ParseAsync(template, model, isHtml).GetAwaiter().GetResult();
     }
   }
 }
--- src/Skelvy.WebAPI/Infrastructure/Emails/IRazorViewToStringRenderer.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Skelvy.WebAPI.Infrastructure.Emails
-{
-  public interface IRazorViewToStringRenderer
-  {
-    Task<string> RenderViewToStringAsync<TModel>(string viewName, TModel model);
-  }
-}
