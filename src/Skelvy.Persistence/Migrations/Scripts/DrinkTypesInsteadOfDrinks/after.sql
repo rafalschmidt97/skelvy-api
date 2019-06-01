bc95787421e8ee9fbf53ce05f333bdd7f2ab0dd7
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-06-01 14:25:01 +0200

    feature(persistence) add drink to drink type migraion

---
+++ src/Skelvy.Persistence/Migrations/Scripts/DrinkTypesInsteadOfDrinks/after.sql
@@ -0,0 +1,21 @@
+﻿IF NOT EXISTS(SELECT *
+              FROM [__EFMigrationsHistory]
+              WHERE [MigrationId] = N'20190531102127_DrinkTypesInsteadOfDrinks')
+  BEGIN
+    DROP INDEX [IX_Meetings_DrinkId] ON [Meetings];
+    ALTER TABLE [Meetings] DROP CONSTRAINT [FK_Meetings_Drinks_DrinkId];
+    ALTER TABLE [Meetings] DROP COLUMN [DrinkId];
+    ALTER TABLE [Meetings] ALTER COLUMN [DrinkTypeId] int NOT NULL;
+    CREATE INDEX [IX_Meetings_DrinkTypeId] ON [Meetings] ([DrinkTypeId]);
+
+    DROP TABLE [MeetingRequestDrinks];
+    DROP TABLE [Drinks];
+
+    DROP TABLE [Meetings_BACKUP];
+    DROP TABLE [MeetingsRequestDrinks_BACKUP];
+    DROP TABLE [Drinks_Backup];
+
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20190531102127_DrinkTypesInsteadOfDrinks', N'2.2.4-servicing-10062');
+  END;
+GO
---
+++ src/Skelvy.Persistence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Skelvy.Persistence.Migrations.Scripts.DrinkTypesInsteadOfDrinks;
+
+namespace Skelvy.Persistence
+{
+  public static class Program
+  {
+    public static void Main()
+    {
+      var migration = new DrinkTypesInsteadOfDrinksMigration(GetConfiguration());
+      migration.Migrate().Wait();
+    }
+
+    private static IConfiguration GetConfiguration()
+    {
+      var basePath = string.Format(
+        "{0}{1}..{1}Skelvy.WebAPI",
+        Directory.GetCurrentDirectory(),
+        Path.DirectorySeparatorChar);
+
+      return new ConfigurationBuilder()
+        .SetBasePath(basePath)
+        .AddJsonFile("appsettings.Development.json")
+        .AddEnvironmentVariables()
+        .Build();
+    }
+  }
+}
