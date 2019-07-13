f3113fdc7bc4dd61b0b393f3b8ae850d78ea947a
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-07-13 16:23:44 +0200

    bugfix(meetings) add conditional validation for chat messages

--- src/Skelvy.Application/Meetings/Commands/AddMeetingChatMessage/AddMeetingChatMessageCommandValidator.cs
+++ src/Skelvy.Application/Meetings/Commands/AddMeetingChatMessage/AddMeetingChatMessageCommandValidator.cs
@@ -7,7 +7,9 @@ namespace Skelvy.Application.Meetings.Commands.AddMeetingChatMessage
     public AddMeetingChatMessageCommandValidator()
     {
       RuleFor(x => x.Message).MaximumLength(500);
+      RuleFor(x => x.Message).NotEmpty().When(x => string.IsNullOrEmpty(x.AttachmentUrl));
       RuleFor(x => x.AttachmentUrl).MaximumLength(2048);
+      RuleFor(x => x.AttachmentUrl).NotEmpty().When(x => string.IsNullOrEmpty(x.Message));
       RuleFor(x => x.UserId).NotEmpty();
     }
   }
