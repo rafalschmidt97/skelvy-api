2ca6fbc071674ca5d09a10977a4d36eca4d6d364
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-08-26 20:28:53 +0300

    refactor(app) remove unnecessary constructor

--- src/Skelvy.Application/Relations/Queries/FriendInvitationsDto.cs
+++ src/Skelvy.Application/Relations/Queries/FriendInvitationsDto.cs
@@ -5,13 +5,6 @@ namespace Skelvy.Application.Relations.Queries
 {
   public class FriendInvitationsDto
   {
-    public FriendInvitationsDto(int id, UserDto invitingUser, DateTimeOffset createdAt)
-    {
-      Id = id;
-      InvitingUser = invitingUser;
-      CreatedAt = createdAt;
-    }
-
     public int Id { get; set; }
     public UserDto InvitingUser { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
