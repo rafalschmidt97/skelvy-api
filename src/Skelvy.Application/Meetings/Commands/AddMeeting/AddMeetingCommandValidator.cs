97e658522579bb247aad1b8dd377c0164acc328b
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-11-27 19:09:18 +0200

    bugfix: size of the group must be between 2..10

--- src/Skelvy.Application/Meetings/Commands/AddMeeting/AddMeetingCommandValidator.cs
+++ src/Skelvy.Application/Meetings/Commands/AddMeeting/AddMeetingCommandValidator.cs
@@ -16,7 +16,11 @@ namespace Skelvy.Application.Meetings.Commands.AddMeeting
       RuleFor(x => x.Latitude).NotEmpty();
       RuleFor(x => x.Longitude).NotEmpty();
       RuleFor(x => x.ActivityId).NotEmpty();
-      RuleFor(x => x.Size).NotEmpty();
+      RuleFor(x => x.Size).NotEmpty()
+        .Must(x => x > 2)
+        .WithMessage("'Size' must be bigger than 2.")
+        .Must(x => x < 10)
+        .WithMessage("'Size' must be smaller than 10.");
     }
   }
 }
